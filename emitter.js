b6a6333284d14e4cc5a7b66af3ce43b79f7ac6f0
Simon Smith <simon@interlopers.net>
2013-12-09 11:15:18 +0000

    Use UMD pattern for compatibility with different module formats

--- emitter.js
+++ emitter.js
@@ -2,114 +2,110 @@
  * @module primish/emitter
  * @description emitter for primish, standalone mediator or mixin
  **/
-;(function(){
+;(function(root, factory){
+	if (typeof define === 'function' && define.amd) {
+		define(['./prime'], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require('./prime'));
+	} else {
+		root.options = factory(root.prime);
+	}
+})(this, function(prime) {
 	'use strict';
 
-	var wrap = function(prime){
-		var slice = Array.prototype.slice;
+	var slice = Array.prototype.slice;
 
-		var EID = 0;
+	var EID = 0;
 
-		var pseudoEvents = {
-			once: function(eventName, fn){
-				var self = this,
-					wrapped = function(){
-					fn.apply(this, arguments);
-					self.off(eventName, wrapped);
-				};
-				return wrapped;
+	var pseudoEvents = {
+		once: function(eventName, fn){
+			var self = this,
+				wrapped = function(){
+				fn.apply(this, arguments);
+				self.off(eventName, wrapped);
+			};
+			return wrapped;
+		}
+	};
+
+	var hideProperty = function(obj, prop){
+		// listeners not to be enumerable where supported
+		return obj[prop] = {}, prime.define(obj, prop, {enumerable: false, value:obj[prop]}), obj[prop];
+	};
+
+	var emitter = prime({
+
+		on: function(event, fn){
+			// supports multiple events split by white space
+			event = event.split(/\s+/);
+			var i = 0,
+				len = event.length,
+				listeners = this._listeners || hideProperty(this, '_listeners'),
+				events,
+				k,
+				pseudos,
+				eventName,
+				knownPseudo;
+
+			loopEvents:
+			for (;i < len; ++i){
+				pseudos = event[i].split(':');
+				eventName = pseudos.shift();
+				knownPseudo = pseudos.length && pseudos[0] in pseudoEvents;
+
+				knownPseudo || (eventName = event[i]);
+				events = listeners[eventName] || (listeners[eventName] = {});
+
+				for (k in events) if (events[k] === fn) continue loopEvents;
+				events[(EID++).toString(36)] = knownPseudo ? pseudoEvents[pseudos[0]].call(this, eventName, fn) : fn;
 			}
-		};
+			return this;
+		},
 
-		var hideProperty = function(obj, prop){
-			// listeners not to be enumerable where supported
-			return obj[prop] = {}, prime.define(obj, prop, {enumerable: false, value:obj[prop]}), obj[prop];
-		};
+		off: function(event, fn){
+			var listeners = this._listeners, events, key, length = 0, l, k;
+			if (listeners && (events = listeners[event])){
 
-		var emitter = prime({
-
-			on: function(event, fn){
-				// supports multiple events split by white space
-				event = event.split(/\s+/);
-				var i = 0,
-					len = event.length,
-					listeners = this._listeners || hideProperty(this, '_listeners'),
-					events,
-					k,
-					pseudos,
-					eventName,
-					knownPseudo;
-
-				loopEvents:
-				for (;i < len; ++i){
-					pseudos = event[i].split(':');
-					eventName = pseudos.shift();
-					knownPseudo = pseudos.length && pseudos[0] in pseudoEvents;
-
-					knownPseudo || (eventName = event[i]);
-					events = listeners[eventName] || (listeners[eventName] = {});
-
-					for (k in events) if (events[k] === fn) continue loopEvents;
-					events[(EID++).toString(36)] = knownPseudo ? pseudoEvents[pseudos[0]].call(this, eventName, fn) : fn;
+				for (k in events){
+					length++;
+					/* don't touch this, === breaks tests due to undefined after delete */
+					if (key == null && events[k] === fn) key = k;
+					if (key && length > 1) break;
 				}
-				return this;
-			},
 
-			off: function(event, fn){
-				var listeners = this._listeners, events, key, length = 0, l, k;
-				if (listeners && (events = listeners[event])){
-
-					for (k in events){
-						length++;
-						/* don't touch this, === breaks tests due to undefined after delete */
-						if (key == null && events[k] === fn) key = k;
-						if (key && length > 1) break;
-					}
-
-					if (key){
-						delete events[key];
-						if (length === 1){
-							// delete so that the order of the array remains unaffected, making it sparse
-							delete listeners[event];
-							for (l in listeners) return this;
-							delete this._listeners;
-						}
+				if (key){
+					delete events[key];
+					if (length === 1){
+						// delete so that the order of the array remains unaffected, making it sparse
+						delete listeners[event];
+						for (l in listeners) return this;
+						delete this._listeners;
 					}
 				}
-				return this;
-			},
+			}
+			return this;
+		},
 
-			trigger: function(event){
-				var listeners = this._listeners,
-					events,
-					k,
-					args = slice.call(arguments, 1),
-					copy = {};
+		trigger: function(event){
+			var listeners = this._listeners,
+				events,
+				k,
+				args = slice.call(arguments, 1),
+				copy = {};
 
-				if (listeners && (events = listeners[event])){
-					for (k in events) copy[k] = events[k];
-					for (k in copy) copy[k].apply(this, args);
-				}
-
-				return this;
+			if (listeners && (events = listeners[event])){
+				for (k in events) copy[k] = events[k];
+				for (k in copy) copy[k].apply(this, args);
 			}
 
-		});
+			return this;
+		}
 
-		emitter.definePseudo = function(pseudo, fn){
-			pseudoEvents[pseudo] = fn;
-		};
+	});
 
-		return emitter;
-	}; // wrap
+	emitter.definePseudo = function(pseudo, fn){
+		pseudoEvents[pseudo] = fn;
+	};
 
-	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd){
-		define(['./prime'], wrap);
-	}
-	else if (typeof module !== 'undefined' && module.exports){
-		module.exports = wrap(require('./prime'));
-	}
-	else {
-		this.emitter = wrap(this.prime);
-	}
-}.call(this));
+	return emitter;
+});
